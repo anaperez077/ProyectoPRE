215309b4f580aeeb6d6676237b9c7acd79a15acd
Paniaguama <107485576+Paniaguama@users.noreply.github.com>
2026-04-30 22:27:03 -0600

    Archivo de conexion

---
+++ ProjectoPRE/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SQLite;
+
+public class Conexion
+{
+    
+    public static string cadena = "Data Source=LibreriaSJ.db;Version=3;";
+
+    public static SQLiteConnection ObtenerConexion()
+    {
+        return new SQLiteConnection(cadena);
+    }
+}
