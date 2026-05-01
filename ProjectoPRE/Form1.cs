66c1bd4d2f9b584896d9db84f5f7f197258d71a5
Paniaguama <107485576+Paniaguama@users.noreply.github.com>
2026-04-30 22:27:03 -0600

    base de datos creada (LibreriaSJ.db) - aplicada ya a base de datos

--- ProjectoPRE/Form1.cs
+++ ProjectoPRE/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace ProjectoPRE
 {
     public partial class Form1 : Form
     {
+        public void CargarVentas()
+        {
+            string conn = "Data Source=LibreriaSJ.db;Version=3;";
+
+            using (var conexion = new SQLiteConnection(conn))
+            {
+                conexion.Open();
+
+                string query = @"SELECT v.id_venta, v.fecha,
+                         IFNULL(SUM(d.cantidad * d.precio), 0) AS total
+                         FROM Ventas v
+                         LEFT JOIN Detalle_Venta d ON v.id_venta = d.id_venta
+                         GROUP BY v.id_venta";
+
+                SQLiteDataAdapter da = new SQLiteDataAdapter(query, conexion);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarVentas();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
