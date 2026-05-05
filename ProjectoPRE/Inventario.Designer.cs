e142dc9a0a9558baf0a20a7c72d9f0c1556433ce
anaperezzz <anamariapc060397@gmail.com>
2026-05-04 18:21:28 -0600

    Agregar el modulo de inventario, funcional totalmente

---
+++ ProjectoPRE/Inventario.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ProjectoPRE
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.Producto = new System.Windows.Forms.Label();
+            this.Descripcion = new System.Windows.Forms.Label();
+            this.Stock = new System.Windows.Forms.Label();
+            this.precio = new System.Windows.Forms.Label();
+            this.Titulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.YellowGreen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(135, 291);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 336);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtNombre.Location = new System.Drawing.Point(473, 76);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(151, 22);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtDescripcion.Location = new System.Drawing.Point(473, 116);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(151, 22);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // txtStock
+            // 
+            this.txtStock.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtStock.Location = new System.Drawing.Point(473, 155);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(151, 22);
+            this.txtStock.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAgregar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAgregar.Location = new System.Drawing.Point(373, 240);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(216, 33);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "+Agregar al inventario\r\n\r\n\r\n";
+            this.btnAgregar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtPrecio.Location = new System.Drawing.Point(473, 194);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(151, 22);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // Producto
+            // 
+            this.Producto.AutoSize = true;
+            this.Producto.Location = new System.Drawing.Point(311, 79);
+            this.Producto.Name = "Producto";
+            this.Producto.Size = new System.Drawing.Size(137, 16);
+            this.Producto.TabIndex = 6;
+            this.Producto.Text = "Nombre del producto:";
+            this.Producto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.AutoSize = true;
+            this.Descripcion.Location = new System.Drawing.Point(366, 122);
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Size = new System.Drawing.Size(82, 16);
+            this.Descripcion.TabIndex = 7;
+            this.Descripcion.Text = "Descripcion:";
+            // 
+            // Stock
+            // 
+            this.Stock.AutoSize = true;
+            this.Stock.Location = new System.Drawing.Point(329, 161);
+            this.Stock.Name = "Stock";
+            this.Stock.Size = new System.Drawing.Size(119, 16);
+            this.Stock.TabIndex = 8;
+            this.Stock.Text = "Cantidad en Stock:";
+            // 
+            // precio
+            // 
+            this.precio.AutoSize = true;
+            this.precio.Location = new System.Drawing.Point(353, 200);
+            this.precio.Name = "precio";
+            this.precio.Size = new System.Drawing.Size(95, 16);
+            this.precio.TabIndex = 9;
+            this.precio.Text = "Precio unitario:";
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Titulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Titulo.Location = new System.Drawing.Point(429, 42);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(133, 16);
+            this.Titulo.TabIndex = 10;
+            this.Titulo.Text = "Gestion de Inventario";
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.CancelButton = this.btnAgregar;
+            this.ClientSize = new System.Drawing.Size(1070, 726);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.precio);
+            this.Controls.Add(this.Stock);
+            this.Controls.Add(this.Descripcion);
+            this.Controls.Add(this.Producto);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Inventario";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.Inventario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label Producto;
+        private System.Windows.Forms.Label Descripcion;
+        private System.Windows.Forms.Label Stock;
+        private System.Windows.Forms.Label precio;
+        private System.Windows.Forms.Label Titulo;
+    }
+}
---
+++ ProjectoPRE/Inventario.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ProjectoPRE
+{
+    public partial class Inventario : Form
+    {
+        // Ruta directa. Asegúrate de que el archivo .db esté en la carpeta bin/Debug
+        private string cadenaConexion = "Data Source=LibreriaSJ.db;Version=3;";
+
+        public Inventario()
+        {
+            InitializeComponent();
+        }
+
+        private void Inventario_Load(object sender, EventArgs e)
+        {
+            CargarInventarioCompleto();
+        }
+
+        public void CargarInventarioCompleto()
+        {
+            try
+            {
+                using (SQLiteConnection conexion = new SQLiteConnection(cadenaConexion))
+                {
+                    conexion.Open();
+
+                    // Usamos LEFT JOIN para que si el producto no tiene inventario, 
+                    // aparezca en la lista con valores en 0 o vacíos.
+                    string query = @"SELECT 
+                                        P.id_producto AS [ID Producto], 
+                                        P.nombre_producto AS [Nombre], 
+                                        IFNULL(I.stock_producto, 0) AS [Stock], 
+                                        IFNULL(I.precio_producto, 0.0) AS [Precio Unitario],
+                                        P.descripcion_producto AS [Descripción]
+                                     FROM Productos P
+                                     LEFT JOIN Inventario I ON P.id_producto = I.id_producto";
+
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(query, conexion);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // VALIDACIÓN BÁSICA
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtPrecio.Text))
+            {
+                MessageBox.Show("Por favor llena el nombre y el precio.");
+                return;
+            }
+
+            try
+            {
+                using (SQLiteConnection conexion = new SQLiteConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    // Iniciamos una transacción para asegurar que se guarde en AMBAS tablas o en ninguna
+                    using (var transaccion = conexion.BeginTransaction())
+                    {
+                        try
+                        {
+                            // 1. Insertar en Productos
+                            string sqlProd = "INSERT INTO Productos (nombre_producto, descripcion_producto) VALUES (@nom, @desc); SELECT last_insert_rowid();";
+                            SQLiteCommand cmd1 = new SQLiteCommand(sqlProd, conexion);
+                            cmd1.Parameters.AddWithValue("@nom", txtNombre.Text);
+                            cmd1.Parameters.AddWithValue("@desc", txtDescripcion.Text);
+
+                            // Obtenemos el ID que se acaba de crear
+                            long idRecienCreado = (long)cmd1.ExecuteScalar();
+
+                            // 2. Insertar en Inventario usando ese ID
+                            string sqlInv = "INSERT INTO Inventario (id_producto, stock_producto, precio_producto) VALUES (@id, @stock, @precio);";
+                            SQLiteCommand cmd2 = new SQLiteCommand(sqlInv, conexion);
+                            cmd2.Parameters.AddWithValue("@id", idRecienCreado);
+                            cmd2.Parameters.AddWithValue("@stock", Convert.ToInt32(txtStock.Text));
+                            cmd2.Parameters.AddWithValue("@precio", Convert.ToDouble(txtPrecio.Text));
+                            cmd2.ExecuteNonQuery();
+
+                            transaccion.Commit();
+                            MessageBox.Show("Producto e Inventario guardados correctamente.");
+
+                            CargarInventarioCompleto(); // Refrescar el grid
+                            LimpiarControles();
+                        }
+                        catch (Exception ex)
+                        {
+                            transaccion.Rollback(); // Si algo falla, deshace los cambios
+                            throw ex;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar datos: " + ex.Message);
+            }
+        }
+
+        private void LimpiarControles()
+        {
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtStock.Clear();
+            txtPrecio.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Por ahora vacío
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //CODIGO CON AYUDA DE IA
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            // 1. Verificamos que los campos no estén vacíos
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtStock.Text) || string.IsNullOrEmpty(txtPrecio.Text))
+            {
+                MessageBox.Show("Por favor, complete los campos obligatorios (Nombre, Stock y Precio).", "Advertencia");
+                return;
+            }
+
+            try
+            {
+                using (SQLiteConnection conexion = new SQLiteConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    // Usamos una transacción: si falla el segundo paso, no se guarda el primero
+                    using (var transaccion = conexion.BeginTransaction())
+                    {
+                        try
+                        {
+                            // PASO A: Insertar en la tabla Productos
+                            string sqlProducto = "INSERT INTO Productos (nombre_producto, descripcion_producto) VALUES (@nom, @desc); SELECT last_insert_rowid();";
+                            SQLiteCommand cmdP = new SQLiteCommand(sqlProducto, conexion);
+                            cmdP.Parameters.AddWithValue("@nom", txtNombre.Text);
+                            cmdP.Parameters.AddWithValue("@desc", txtDescripcion.Text);
+
+                            // Ejecutamos y obtenemos el ID que la base de datos le asignó al producto
+                            long nuevoId = (long)cmdP.ExecuteScalar();
+
+                            // PASO B: Insertar en la tabla Inventario usando ese ID
+                            string sqlInventario = "INSERT INTO Inventario (id_producto, stock_producto, precio_producto) VALUES (@id, @stock, @precio)";
+                            SQLiteCommand cmdI = new SQLiteCommand(sqlInventario, conexion);
+                            cmdI.Parameters.AddWithValue("@id", nuevoId);
+                            cmdI.Parameters.AddWithValue("@stock", int.Parse(txtStock.Text));
+                            cmdI.Parameters.AddWithValue("@precio", double.Parse(txtPrecio.Text));
+                            cmdI.ExecuteNonQuery();
+
+                            // Si llegamos aquí sin errores, confirmamos los cambios
+                            transaccion.Commit();
+
+                            MessageBox.Show("Producto guardado correctamente en el Inventario", "Éxito");
+
+                            // Limpiamos los cuadros de texto y refrescamos la tabla visual
+                            LimpiarControles();
+                            CargarInventarioCompleto();
+                        }
+                        catch (Exception ex)
+                        {
+                            transaccion.Rollback(); // Si algo falló, deshace todo para no dejar datos corruptos
+                            MessageBox.Show("Error al guardar en la base de datos: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+            }
+
+
+        }
+    }
+
+}
+
